7dc0a05905b51854ff5ab22d888ea414efde9621
akshaya-sidmec <akshaya@sidmectech.in>
2025-06-30 14:58:05 +0530

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Car Washing Management System",
+    "author":"sidmec_interns",
+     "license": "LGPL-3",
+    "version": "18.0.1.1",
+    "depends":['base','contacts','account','stock','hr','product','mail'],
+    "data": [ ]
+}

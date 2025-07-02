36f2ec986cd6a2a4cb26d9e65a42b088d119b9fd
Adithya S Kumar <adithyask880@gmail.com>
2025-07-02 10:28:35 +0530

    update in car wash job

--- sdm_car_mgmt_project/__manifest__.py
+++ sdm_car_mgmt_project/__manifest__.py
@@ -22,8 +22,12 @@
         "security/ir.model.access.csv",
         "views/car_wash_views.xml",
         "views/service_category.xml",
+<<<<<<< Updated upstream
         # "wizard/wizard.xml",
         "data/email_template.xml",
+=======
+        "views/car_wash_job_views.xml",
+>>>>>>> Stashed changes
         "views/menu.xml",
     ],
 
--- sdm_car_mgmt_project/models/__init__.py
+++ sdm_car_mgmt_project/models/__init__.py
@@ -1,2 +1,3 @@
 from . import car_wash_booking
 from . import service_categories
+from . import car_wash_job
